39a148471b132443f989c5b090c5d9161c678987
elaaarfaoui <elaa.arfaoui1@gmail.com>
2021-02-05 23:11:40 +0100

    first commit

---
+++ src/Components/JS/Reducers/todoReducer.js
@@ -0,0 +1,60 @@
+import {
+  ADD_TODO,
+  EDIT_TODO,
+  COMPLETE_TODO,
+} from "../ActionsTypes/actionsTypes";
+
+const initialState = {
+  todoList: [
+    {
+      text: "My First Todo",
+      id: 0,
+      isDone: false,
+    },
+
+    {
+      text: "My Seconde Todo",
+      id: 1,
+      isDone: false,
+    },
+
+    {
+      text: "My Third Todo",
+      id: 2,
+      isDone: false,
+    },
+  ],
+};
+
+const todoReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ADD_TODO:
+      return {
+        ...state,
+
+        todoList: [...state.todoList, payload],
+      };
+
+    case EDIT_TODO:
+      return {
+        ...state,
+
+        todoList: state.todoList.map((todo) =>
+          todo.id === payload.id ? { ...todo, text: payload.text } : todo
+        ),
+      };
+
+    case COMPLETE_TODO:
+      return {
+        ...state,
+        todoList: state.todoList.map((todo) =>
+          todo.id === payload ? { ...todo, isDone: !todo.isDone } : todo
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
